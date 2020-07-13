35f9771387ba15b3c23f23bc02ff8976b1d5d175
DrgRol <dvdrol@gmail.com>
2020-07-13 12:01:12 +0200

    deploy heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MarcoOrquestador
+# contenedor
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'marco-Orquestador'`, () => {
+  it(`should have as title 'contenedor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('marco-Orquestador');
+    expect(app.title).toEqual('contenedor');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('marco-Orquestador app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('contenedor app is running!');
   });
 });
